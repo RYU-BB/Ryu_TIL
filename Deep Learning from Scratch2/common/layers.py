3897bb8d459f1642296cedf09a04f7d1cea441c6
Myeonghyeon Ryu <59256704+RYU-BB@users.noreply.github.com>
2021-03-21 18:24:05 +0900

    Update layers.py

--- Deep Learning from Scratch2/common/layers.py
+++ Deep Learning from Scratch2/common/layers.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('..')
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 
 class MatMul:
@@ -20,4 +21,31 @@ class MatMul:
         dx = np.matmul(dout, W.T)
         dW = np.matmul(self.x.T, dout)
         self.grads[0][...] = dW  # 깊은 복사
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
         return dx
