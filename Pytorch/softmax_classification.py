84e740c149c797d8f7ebe206b8133f0ae55e5790
RYU-BB <tmxkdd5790@gmail.com>
2021-05-16 20:33:24 +0900

    Update softmax_classification.py

--- Pytorch/softmax_classification.py
+++ Pytorch/softmax_classification.py
@@ -1,3 +1,16 @@
+"""
+softmax_classification
+
+Discrete Probability Distribution
+Y값을 확률적으로 계산
+ex) ([1, 2, 3]) -> ([0.09, 0.2447, 0.6652]) 합이 1
+
+P(class = i)= e^i / sigma(e^i)
+
+Cross Entropy
+두 확률분포가 얼마나 비슷한지
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,3 +23,65 @@ z = torch.FloatTensor([1, 2, 3])
 
 hypothesis = F.softmax(z, dim=0)
 print(hypothesis)
+
+# size = (3, 5)
+z = torch.rand(3, 5, requires_grad=True)
+hypothesis = F.softmax(z, dim=1)
+print(hypothesis)
+
+y = torch.randint(5, (3,)).long()
+print(y)
+
+# size = (3, 5)
+y_one_hot = torch.zeros_like(hypothesis)
+y_one_hot.scatter_(1, y.unsqueeze(1), 1)
+
+# cost
+cost = (y_one_hot * -torch.log(hypothesis)).sum(dim=1).mean()
+print(cost)
+
+# cross-entropy Loss with torch.nn.functional
+F.nll_loss(F.log_softmax(z, dim=1), y)
+
+F.cross_entropy(z, y)
+
+# |x_train| = (m, 4)
+# |y_train| = (m,)
+x_train = [[1, 2, 1, 1],
+           [2, 1, 3, 2],
+           [3, 1, 3, 4],
+           [4, 1, 5, 5],
+           [1, 7, 5, 5],
+           [1, 2, 5, 6],
+           [1, 6, 6, 6],
+           [1, 7, 7, 7]]
+y_train = [2, 2, 2, 1, 1, 1, 0, 0]
+x_train = torch.FloatTensor(x_train)
+y_train = torch.FloatTensor(y_train)
+
+W = torch.zeros((4, 3), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+optimizer = optim.SGD([W, b], lr=0.1)
+
+nb_epochs = 1000
+for epoch in range(nb_epochs + 1):
+
+    z = x_train.matmul(W) + b
+    cost = F.cross_entropy(z, y_train)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+        print("Epoch {:4d}/{} Cost: {:.6f}".format(epoch, nb_epochs, cost.item()))
+
+
+class SoftmaxClassifierModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(4, 3)
+
+    def forward(self, x):
+        return self.linear(x)
